a566709cbba7ffdfcfe548284015c7a2e0acda62
Waltton Morais <waltton@2xt.com.br>
2019-04-18 15:21:10 -0300

    Fix client add_event

--- pgevstore/client/__init__.py
+++ pgevstore/client/__init__.py
@@ -1,5 +1,7 @@
-import psycopg2
+import json
 import uuid
+
+import psycopg2
 
 
 class Client:
@@ -9,7 +11,7 @@ class Client:
     def __del__(self):
         self.conn.close()
 
-    def add_event(self, source, ev_type, description, data, tags, ev_time):
+    def add_event(self, source, ev_type, description, data, tags, ev_time=None):
         if not source:
             raise Exception('The "source" value should not be empty')
 
@@ -18,22 +20,24 @@ class Client:
 
         key = uuid.uuid4()
 
-        with self.conn() as cur:
+        with self.conn.cursor() as cur:
             cur.execute("""
             INSERT INTO events(key, source, ev_type, description, data, tags, ev_time)
-            SELECT {key}
-                 , {source}
-                 , {ev_type}
-                 , {description}
-                 , {data}
-                 , {tags}
-                 , CASE WHEN {ev_time} IS NOT NULL THEN {ev_time} ELSE now() END
+            SELECT %(key)s
+                 , %(source)s
+                 , %(ev_type)s
+                 , %(description)s
+                 , %(data)s
+                 , %(tags)s
+                 , CASE WHEN %(ev_time)s IS NOT NULL THEN %(ev_time)s ELSE now() END
             """, {
                 "key": key,
                 "source": source,
                 "ev_type": ev_type,
                 "description": description,
-                "data": data,
+                "data": json.dumps(data),
                 "tags": tags,
                 "ev_time": ev_time,
             })
+
+        self.conn.commit()
